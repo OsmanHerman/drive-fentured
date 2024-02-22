4676b6c0b306e7bb2f69f71c4bf90b3a78cee0cb
Osman Herman <113738737+OsmanHerman@users.noreply.github.com>
2024-02-21 22:24:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".featured_Swiper", {
+  //spaceBetween: 40,
+  effect: "cards",
+  grabCursor: true,
+  //perSlideOffset: 2,
+  //perSlideRotate: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
